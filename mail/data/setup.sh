5cda155b0ad17b892013ad089926353cdedbb1ad
zwarmex <jpollart@gmail.com>
2024-03-24 19:13:01 +0000

    initialisation TP07

---
+++ mail/data/setup.sh
@@ -0,0 +1,6 @@
+docker exec -ti mail setup email add samaze123@l1-3.ephec-ti.be
+docker exec -ti mail setup email add zwarmex@l1-3.ephec-ti.be
+
+docker exec -ti mail setup alias add postmaster@l1-3.ephec-ti.be samaze123@l1-3.ephec-ti.be
+
+
